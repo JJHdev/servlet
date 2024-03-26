5e7edc71a364b9f3df2d379bb072e0ab59e5c3b2
JJHdev <goodjob321@hanmail.net>
2024-03-26 17:29:09 +0900

    1st commit

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,11 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+
+}
